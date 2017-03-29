ac90e75591d17a5346dc72f699d0b7e5ddb57fd7
Matthew Kulina <Matthew@Matthews-MacBook-Pro.local>
2017-03-29 11:50:11 -0400

    add react

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,23 @@
 console.log('Hello Freshwater');
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+
+  times10(number) {
+    return number * 10
+  }
+
+  render () {
+    var number = 100;
+
+    return (
+      <div>
+        <p> Hello React!</p>
+        <h2> OK OK { this.times10(number) }</h2>
+      </div>
+    );
+  }
+}
+
+render(<App/>, document.getElementById('app'));
