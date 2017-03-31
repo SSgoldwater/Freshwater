0480d89f62535dbd309498915ee90b3e5fabdf33
Stanley <stanleyjsdz@gmail.com>
2017-03-30 21:17:31 -0400

    add cordova build

---
+++ src/utils/buildCordova.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+./node_modules/.bin/webpack -d -p --config webpack.config.cordova.js
+
+INDEX="./fwcordova/www/js/index.js"
+
+rm -rf $INDEX
+
+FILE_PREFIX="
+var app = {\n
+    // Application Constructor\n
+    initialize: function() {\n
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);\n
+    },\n\n
+
+    // deviceready Event Handler\n
+    //\n
+    // Bind any cordova events here. Common events are:\n
+    // 'pause', 'resume', etc.\n
+    onDeviceReady: function() {\n
+"
+
+FILE_SUFFIX="
+    }\n
+  },\n\n
+
+app.initialize();
+"
+
+echo $FILE_PREFIX > $INDEX
+cat ./fwcordova/www/js/bundle.js >> $INDEX
+echo $FILE_SUFFIX >> $INDEX
