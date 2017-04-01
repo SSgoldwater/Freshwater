9cfe729a4a99492e60dab2d6ec1977f4fcdd549f
Stanley <stanleyjsdz@gmail.com>
2017-03-31 21:49:24 -0400

    add postgres framework to Precipitation

--- Precipitation/Server.js
+++ Precipitation/Server.js
@@ -2,9 +2,22 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var precipitation = express();
 var router = express.Router();
+var pg = require('pg')
 
-router.get('/', function(req, res) {
-  res.json({ message: "Making it pour" });
+var client = new pg.Client({ database: "freshwater_dev" });
+
+router.get('/users', function(req, res) {
+  client.connect(function(err) {
+    if (err) throw err;
+    client.query("SELECT * FROM users", function(err, result) {
+      if (err) {
+        throw err;
+      } else {
+        console.log(result.rows);
+        res.json({ rows: result.rows });
+      }
+    });
+  });
 });
 
 precipitation.use('/api', router);
