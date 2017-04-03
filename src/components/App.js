04cdf078926631cb5e54623a2c7f77f0f2dab575
Stanley <stanleyjsdz@gmail.com>
2017-04-03 00:01:14 -0400

    move muithemeprovider to router

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Navigation from './layout/Navigation';
 
 class App extends React.Component {
   render () {
     return (
-      <MuiThemeProvider>
         <div>
           <Navigation/>
           {this.props.children}
         </div>
-      </MuiThemeProvider>
     );
   }
 };
--- src/utils/Router.js
+++ src/utils/Router.js
@@ -1,5 +1,6 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import App from '../components/App';
 import Main from '../components/main/Main';
 import WaterDash from '../components/waterdash/WaterDash';
@@ -8,16 +9,18 @@ import Rewards from '../components/rewards/Rewards';
 import Login from '../components/layout/Login';
 
 const FWRouter = (
-  <Router>
-    <div>
-      <Route path="/" component={ App }/>
-      <Route path="/main" component={ Main }/>
-      <Route path="/todo" component={ ToDo }/>
-      <Route path="/waterdash" component={ WaterDash }/>
-      <Route path="/rewards" component={ Rewards }/>
-      <Route path="/login" component={ Login }/>
-    </div>
-  </Router>
+  <MuiThemeProvider>
+    <Router>
+      <div>
+        <Route path="/" component={ App }/>
+        <Route path="/main" component={ Main }/>
+        <Route path="/todo" component={ ToDo }/>
+        <Route path="/waterdash" component={ WaterDash }/>
+        <Route path="/rewards" component={ Rewards }/>
+        <Route path="/login" component={ Login }/>
+      </div>
+    </Router>
+  </MuiThemeProvider>
 );
 
 export default FWRouter;
